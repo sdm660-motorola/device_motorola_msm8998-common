aa7dfd0cbbfe38755965c69038408bf180221e76
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    msm8998-common: Create dummy for libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    [SebaUbuntu]: Cleanup Android.bp and add copyright header
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- common.mk
+++ common.mk
@@ -305,6 +305,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.motorola-libperfmgr
